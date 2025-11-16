e816db046d4718c5913eb762c2be98b6f57d420f
Aisha Attar <aishaat@student.chalmers.se>
2025-11-16 17:40:53 +0100

    #1 Handle personalNumber edge case, Contribution: @aishaat @ajamieh @arifab

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
   { 
     userId: { type: String, unique: true, immutable : true},        
-    personalNumber: { type: String, required: true, unique: true , immutable: true},    
+    personalNumber: { type: String, required: true, unique: true , 
+      immutable: true, 
+      match: [/^\d{10}$/, 'Personal number must be exactly 10 digits']},    
     language: { type: String, default: 'en' },
     password: { type: String, required: true }
   },
