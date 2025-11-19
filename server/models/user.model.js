753f5a1da20f67cdd65f2610ddf272f49e8d0f14
sireen <ajamieh@student.chalmers.se>
2025-11-19 23:29:23 +0100

    #1 Add password hashing

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema(
   { 
@@ -11,5 +12,22 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre('save', async function save(next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(8);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (err) {
+        next(err);
+  }
+});
+
+userSchema.methods.validatePassword = async function validatePassword(loginPassword) {
+  return bcrypt.compare(loginPassword, this.password);
+};
+
 
 module.exports = mongoose.model('User', userSchema);
--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.model');
 
-// POST /api/users
-router.post('/', async (req, res, next) => {
+// POST /api/userslregister
+router.post('/register', async (req, res, next) => {
   try {
+    const {password} = req.body;
+    if (!password || password.length < 8) {
+      return res.status(400).json({
+        message: "Password must be at least 8 characters long"
+      });
+    }
     const user = await User.create(req.body);
     res.status(201).json(user);
   } catch (err) {
@@ -12,16 +18,25 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-// GET /api/users
-router.get('/', async (req, res, next) => {
-    try {
-      const users = await User.find();
-      res.status(201).json(users);
-    } catch (err) {
-      next(err);
-      res.status(404).json({ message: 'Not Found' });
+// POST /api/users/login
+router.post('/login', async (req, res, next) => {
+  try {
+    const { userId, password } = req.body;
+    const user = await User.findOne({ userId });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
     }
-  });
+    const checkPassword = await user.validatePassword(password);
+    if (!checkPassword) {
+      return res.status(401).json({ message: 'Incorrect Password' });
+}
+    res.status(200).json({
+    message: "Login successful"
+});
+  } catch (err) {
+    next(err);
+  }
+});
 
 // GET /api/users:UserId
 router.get('/:userId', async (req, res, next) => {
