5b100258886ebc2ef85819e6cb51ac7362a48d28
Aisha Attar <aishaat@student.chalmers.se>
2025-11-19 20:18:54 +0100

    #2 Update the message model with a body attribute, reactions and foreign keys.

--- server/app.js
+++ server/app.js
@@ -18,7 +18,10 @@ app.use(cors());
 
 // routes
 const userRoutes = require('./routes/users.routes');
+const messageRoutes = require('./routes/messages.routes');
 const branchingRoomRoutes = require('./routes/branchingroom.routes');
+const localRoomRoutes = require('./routes/localrooms.routes');
+
 
 // greeting route
 app.get('/api', function (req, res) {
@@ -27,7 +30,9 @@ app.get('/api', function (req, res) {
 
 // mount routes
 app.use('/api/users', userRoutes);
+app.use('/api/messages', messageRoutes);
 app.use('/api/branchingrooms',branchingRoomRoutes);
+app.use('/api/localrooms', localRoomRoutes);
 
 // 404 fallback
 app.use('/api/*', function (req, res) {
--- server/models/message.model.js
+++ server/models/message.model.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-  MessageId: { type: String },
-  SendTimestamp: { type: Date },
-  Reaction: { type: String },
-  ResponseIds: { type: [Number] },
-  UserId: { type: String }
-});
+  messageId: { type: String, unique: true, required: true, immutable: true, index:true },
+  Body: {type: String, required: true },
+  SendTimestamp: { type: Date }, 
+  Reaction: { type: String, 
+    enum: ['👍', '❤️', '😂', '😢', '😡', null] 
+  }, 
+  ResponseIds: [{ type: mongoose.Schema.ObjectId, ref: 'messages', required: false }], 
+  //Foreign key Refferences
+  Sender: { type: Schema.Types.ObjectId, ref: 'User' }, 
+  BranchingRoom: { type: Schema.Types.ObjectId, ref: 'branchingRoom' }
+}); 
 
 module.exports = mongoose.model('messages', messageSchema);
