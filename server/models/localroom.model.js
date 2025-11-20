753465c8ebe34de948b6c4318681cdea67027c99
Abdullah Arif <arifab@chalmers.se>
2025-11-20 22:19:34 +0100

    #6 Update Local Room Schema

--- server/models/localroom.model.js
+++ server/models/localroom.model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 
 const LocalRoomSchema  = new mongoose.Schema({
-    roomId : {type: String, required: [true, "Name is Required"], unique: true, immutable : true},
-    topicDropdown : {type :[String], enum:["School", "Food", "Hobbies"], immutable : true},
+    roomId : {type: String, required: [true, "Id is Required"], unique: true, immutable : true},
+    topicDropdown : {type :[String], enum:["General","School", "Food", "Hobbies"], immutable : true, default: "General"},
     country : {type: String},
     liveChat: {type: Boolean, required: true}
 
