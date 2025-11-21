a3d940465e824816d54d439b45d9885d4c193d75
sireen <ajamieh@student.chalmers.se>
2025-11-21 12:21:33 +0100

    #4 Modify global routes

--- server/routes/global.routes.js
+++ server/routes/global.routes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const Global = require('../models/global.model');
+const GlobalRoom = require('../models/global.model');
 
 // POST /api/global
 router.post('/', async (req, res, next) => {
   try {
-    const roomExists = await Global.findOne();
+    const roomExists = await GlobalRoom.findOne();
     if (roomExists){ 
         return res.status(404).json({message:"Global room already exists"});
     }
-    const room = await Global.create(req.body);
+    const room = await GlobalRoom.create(req.body);
     res.status(201).json(room);
 } catch (err) {
   next(err);
@@ -19,8 +19,8 @@ router.post('/', async (req, res, next) => {
 // GET /api/globalRoom
 router.get('/', async (req, res, next) => {
     try {
-      const globalRoom = await Global.findOne();
-      res.status(201).json(globalRoom);
+      const globalRoom = await GlobalRoom.findOne();
+      res.status(200).json(globalRoom);
     } catch (err) {
       next(err);
       res.status(404).json({ message: 'Not Found' });
@@ -30,7 +30,7 @@ router.get('/', async (req, res, next) => {
 // update /api/live_chat
 router.patch("/" ,async (req, res)=>{
     try{
-        const updatedRoom = await Global.findOneAndUpdate({$set:{live_Chat: req.body.live_Chat}});
+        const updatedRoom = await GlobalRoom.findOneAndUpdate({},{$set:{live_Chat:req.body.live_Chat}},{new:true});
         res.status(200).json({message : "Success",updatedRoom});
     }catch(err){
         res.status(404).json({message:"Not Found", "error" : err})
@@ -40,7 +40,7 @@ router.patch("/" ,async (req, res)=>{
 // Delete /api/Global/:room_Id
 router.delete("/:room_Id" ,async (req, res)=>{
     try{
-        const deletedRoom = await Global.findOneAndDelete({room_Id: req.params.room_Id});
+        const deletedRoom = await GlobalRoom.findOneAndDelete({room_Id: req.params.room_Id});
         res.status(200).json({message : "Success",deletedRoom});
     }catch(err){
         res.status(500).json({message:"Not Found"})
