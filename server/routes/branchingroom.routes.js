c52d6f33737a06a7676f9a24b65a99bd21ffa945
Abdullah Arif <arifab@chalmers.se>
2025-11-21 16:19:46 +0100

    #7 Update routes to move the business logic

--- server/routes/branchingroom.routes.js
+++ server/routes/branchingroom.routes.js
@@ -1,99 +1,45 @@
 const express = require("express");
 const BranchingRoom = require("../models/branchingroom.model");
+const messagesModel = require("../models/message.model");
+const branchingroomModel = require("../models/branchingroom.model");
 const branchingRoomRoute  = express.Router();
+const branchingRoomController = require('../controllers/branchingrooms.controller');
 
 
 
 // Create One Brancing Rooms
-branchingRoomRoute.post("/", async (req, res, next)=>{
-    try{
-        const SingleBrancingRooms = await BranchingRoom.create(req.body);
-        const BrancingRooms = await SingleBrancingRooms.find(SingleBrancingRooms.parentRoomId).populate("parentRoomId");
+branchingRoomRoute.post("/", branchingRoomController.createBranchingRoom);
 
-        res.status(201).json({message: "Success", Object: BrancingRooms});
-        next();
-    }catch(err){
-        res.status(422).json({message: "Not Found", Error_Message: err});
-        next(err);
-    } 
+// POST /branchingroom/:branchingroomId/message
+branchingRoomRoute.post("/:branchingRoomId/messages", branchingRoomController.createMessageInABranchingRoom);
 
-});
+// GET: Read All Branching Rooms
+branchingRoomRoute.get("/", branchingRoomController.getAllBranchingRooms);
 
+// GET: Read One Branching Rooms SuperType
+branchingRoomRoute.get("/:branchingRoomId", branchingRoomController.getBranchingRoom);
 
-// Read All Branching Rooms
-branchingRoomRoute.get("/", async (req, res, next)=>{
-    try{
-        const allBrancingRooms = await BranchingRoom.find(req.body);
-        res.status(200).json(allBrancingRooms);
-               
-    }catch(err){
-        res.status(404).json({message: "Not Found", Error_Message: err});
-        
+// GET: All Messages belonging to a branching room
+branchingRoomRoute.get("/:branchingRoomId/messages", branchingRoomController.getAllMessagesInBranchingRoom);
 
-    } 
+// GET: Specific Message from a specific branching room
+branchingRoomRoute.get("/:branchingRoomId/messages/:messageId",branchingRoomController.getAMessageInABranchingRoom);
 
-});
+// PATCH: Update One Branhing Room
+branchingRoomRoute.patch("/:branchingRoomId", branchingRoomController.updateBranchingRoomTopic);
+
+// PATCH: Edit a specific message, reaction, response
+branchingRoomRoute.patch("/:branchingRoomId/messages/:messageId", branchingRoomController.updateMessageInBranchingRoom);
 
 
-// Read Branching Room By Id
-branchingRoomRoute.get("/:branchingRoomId", async (req, res)=>{
-    try{
-        const SingleBranchingRoom = await BranchingRoom.findOne({branchingRoomId: req.params.branchingRoomId}).populate("parentRoomId");
-        if (!SingleBranchingRoom) {
-            return res.status(404).json({ message: "Branching room not found" });
-    }
-        res.status(200).json(SingleBranchingRoom);
-    }catch(err){
-        res.status(404).json({message: "Not Found", Error_Message: err.message});
+// DELETE:  One Branching Room
+branchingRoomRoute.delete("/:branchingRoomId", branchingRoomController.deleteBranchingRoom);
 
-    } 
+// DELETE: All Branching Room Collection
+branchingRoomRoute.delete("/", branchingRoomController.deleteAllBranchingRooms);
 
-});
-
-// Read One Branching Rooms SuperType
-branchingRoomRoute.get("/:branchingRoomId", async (req, res, next)=>{
-    try{
-        const SingleBranchingRooms = await BranchingRoom.findOne({branchingRoomId: req.params.branchingRoomId}).populate("parentRoomId").exec();
-        res.status(200).json(SingleBranchingRooms);
-    
-    }catch(err){
-        res.status(404).json({message: "Not Found", Error_Message: err.message});
-    
-    } 
-
-});
-
-
-// Update One Branhing Room
-branchingRoomRoute.patch("/:branchingRoomId", async (req, res, next)=>{
-    try{
-        const updatedRoom = await BranchingRoom.findOneAndUpdate({branchingRoomId: req.params.branchingRoomId}, req.body, {new: true, runValidators: true});
-        if(!updatedRoom){res.status(404).json({message: "Not Found"});}
-        res.status(200).json(SingleBranchingRooms);
-        next();
-    }catch(err){
-        res.status(404).json({message: "Not Found", Error_Message: err});
-        next(err);
-       
-    } 
-
-});
-
-
-// Delete One Branching Room
-branchingRoomRoute.delete("/:branchingRoomId", async (req, res)=>{
-    try{
-        const deletedRoom = await BranchingRoom.findOneAndDelete({branchingRoomId: req.params.branchingRoomId});
-        if(!deletedRoom){ res.status(404).json({message: " Not Found"})}
-        res.status(200).json({message: "Successfully Deleted"});
-        next();
-    }catch(err){
-        res.status(404).json({message: "Not Found", "Error_Message": err});
-        next(err);
-    } 
-
-});
-
+// DELETE: A specific message in a specific branching room
+branchingRoomRoute.delete("/:branchingRoomId/messages/:messageId", branchingRoomController.deleteMessageInBranchingRoom);
 
 module.exports = branchingRoomRoute;
 
