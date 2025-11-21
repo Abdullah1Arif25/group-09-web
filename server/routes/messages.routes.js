7910b8e818612d19c8d290b3520dee2d50ec6157
Abdullah Arif <arifab@chalmers.se>
2025-11-21 15:48:50 +0100

    #2 Remove Business Logic from Routes
    Contribution: @aishaat

--- server/routes/messages.routes.js
+++ server/routes/messages.routes.js
@@ -1,82 +1,27 @@
 const express = require('express');
 const MessagesRoute = express.Router();
 const Message = require('../models/message.model');
+const MessageController = require('../controllers/messages.controllers');
 
 
 // POST createMessage
-MessagesRoute.post('/', async (req, res, next)  =>{
-  
-  try {
-    const newMessage = await Message.create(req.body);
-  
-
-    res.send(201).json({message:"Successs", "Object": populatedMessage});
-    
-  } catch (err) {
-    next(err);
-  }
-  
-});
+MessagesRoute.post('/', MessageController.createMessage);
 
 
 // GET getAllMessages
-MessagesRoute.get('/', async (req, res, next)  =>{
-  try {
-    const messages = await Message.find();
-    res.status(200).json(messages);
-  } catch (err) {
-    next(err);
-  }
-});
+MessagesRoute.get('/', MessageController.getAllMessages);
 
 
 // GET getMessageById
-MessagesRoute.get('/:messageId', async (req, res, next) =>{
-  try {
-    const message = await Message.findOne({messageId:req.params.messageId}).populate('Sender').populate('BranchingRoom').populate('ResponseIds');
-    if (!message) {
-      return res.status(404).json({ error: 'Message not found' });
-    }
-    res.status(200).json(message);
-  } catch (err) {
-    next(err);
-  }
-});
+MessagesRoute.get('/:messageId', MessageController.getMessageById);
 
 
 
 // PATCH patchMessage
-MessagesRoute.patch('/:messageId', async (req, res, next)  =>{
-  try {
-    const patchedMessage = await Message.findOneAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true, runValidators: true }
-    );
-
-    if (!patchedMessage) {
-      return res.status(404).json({ error: 'Message not found' });
-    }
-    res.status(200).json(patchedMessage);
-  } catch (err) {
-    next(err);
-  }
-});
+MessagesRoute.patch('/:messageId', MessageController.updateMessageById);
 
 
 // DELETE deleteMessage
-MessagesRoute.delete('/:messageId', async (req, res, next)  =>{
-  try {
-    const deletedMessage = await Message.findOneAndDelete({messageId: req.params.messageId});
-
-    if (!deletedMessage) {
-      return res.status(404).json({ error: 'Message not found' });
-    }
-
-    res.status(200).json({message: "Successs"});
-  } catch (err) {
-    next(err);
-  }
-});
+MessagesRoute.delete('/:messageId', MessageController.deleteMessageById);
 
 module.exports = MessagesRoute;
