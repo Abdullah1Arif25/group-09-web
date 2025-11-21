4bba554bbacc0a319ac24a6a42c417a9626050f1
sireen <ajamieh@student.chalmers.se>
2025-11-22 00:44:44 +0100

    #1 Modify the endpoints in controller file and the routes in route file

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user.model');
 const UserController = require('../controllers/users.controller');
 
-// POST /api/userslregister
+// POST /api/users/register
 router.post('/register', UserController.registerUser);
 
 // POST /api/users/login
 router.post('/login',UserController.loginUser);
 
-// GET /api/users:UserId
-router.get('/:userId', UserController.getAUser);
+// GET /api/users
+router.get('/',UserController.getAllUsers);
 
+// GET /api/users/:UserId
+router.get('/:userId', UserController.getAUser);
 
 // update /api/users/:userId
 router.patch("/:userId" ,UserController.updateAUser);
