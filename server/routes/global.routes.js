b8ce7c158f0ad2a045b45ebcf2b073fbb5374eeb
sireen <ajamieh@student.chalmers.se>
2025-11-22 03:11:58 +0100

    #4 Fix pipeline error

--- server/routes/global.routes.js
+++ server/routes/global.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {createGlobalRoom, getGlobalRoom, updateGlobalRoom, deleteGlobalRoom} = require('../controllers/global.controller');
+const {createGlobalRoom, getGlobalRoom, updateGlobalRoom, deleteGlobalRoom} = require('../controllers/global.controllers');
 
 // POST /api/global
 router.post('/', createGlobalRoom);
