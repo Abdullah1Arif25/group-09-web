cb4841d4401a5001fa657a4eb326d1a3b4802c23
sireen <ajamieh@student.chalmers.se>
2025-11-22 03:08:22 +0100

    #4 Add controllers file for global

---
+++ server/controllers/global.controllers.js
@@ -0,0 +1,67 @@
+const GlobalRoom = require('../models/global.model');
+
+// Create global room
+const createGlobalRoom = async function (req, res, next) {
+  try {
+    const roomExists = await GlobalRoom.findOne();
+    if (roomExists) {
+      return res.status(400).json({ message: "Global room already exists." });
+    }
+
+    const room = await GlobalRoom.create(req.body);
+    return res.status(201).json(room);
+
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// Get global room
+const getGlobalRoom = async function (req, res, next) {
+  try {
+    const globalRoom = await GlobalRoom.findOne();
+
+    if (!globalRoom) {
+      return res.status(404).json({ message: "Global room not found." });
+    }
+
+    return res.status(200).json(globalRoom);
+
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// Update live chat status
+const updateGlobalRoom = async function (req, res, next) {
+  try {
+    const updatedRoom = await GlobalRoom.findOneAndUpdate({}, { $set: { live_Chat: req.body.live_Chat }}, { new: true });
+
+    if (!updatedRoom) {
+      return res.status(404).json({ message: "Global room not found." });
+    }
+
+    return res.status(200).json({ message: "Success."});
+
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// Delete global room
+const deleteGlobalRoom = async function (req, res, next) {
+  try {
+    const deletedRoom = await GlobalRoom.findOneAndDelete({room_Id: req.params.room_Id});
+
+    if (!deletedRoom) {
+      return res.status(404).json({ message: "Global room not found." });
+    }
+
+    return res.status(200).json({ message: "Successully deleted."});
+
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports = {createGlobalRoom, getGlobalRoom, updateGlobalRoom, deleteGlobalRoom};
--- server/routes/global.routes.js
+++ server/routes/global.routes.js
@@ -1,50 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const GlobalRoom = require('../models/global.model');
+const {createGlobalRoom, getGlobalRoom, updateGlobalRoom, deleteGlobalRoom} = require('../controllers/global.controller');
 
 // POST /api/global
-router.post('/', async (req, res, next) => {
-  try {
-    const roomExists = await GlobalRoom.findOne();
-    if (roomExists){ 
-        return res.status(404).json({message:"Global room already exists"});
-    }
-    const room = await GlobalRoom.create(req.body);
-    res.status(201).json(room);
-} catch (err) {
-  next(err);
-  }
-});
+router.post('/', createGlobalRoom);
 
 // GET /api/globalRoom
-router.get('/', async (req, res, next) => {
-    try {
-      const globalRoom = await GlobalRoom.findOne();
-      res.status(200).json(globalRoom);
-    } catch (err) {
-      next(err);
-      res.status(404).json({ message: 'Not Found' });
-    }
-  });
+router.get('/', getGlobalRoom);
 
 // update /api/live_chat
-router.patch("/" ,async (req, res)=>{
-    try{
-        const updatedRoom = await GlobalRoom.findOneAndUpdate({},{$set:{live_Chat:req.body.live_Chat}},{new:true});
-        res.status(200).json({message : "Success",updatedRoom});
-    }catch(err){
-        res.status(404).json({message:"Not Found", "error" : err})
-    }
-})
+router.patch('/', updateGlobalRoom);
 
 // Delete /api/Global/:room_Id
-router.delete("/:room_Id" ,async (req, res)=>{
-    try{
-        const deletedRoom = await GlobalRoom.findOneAndDelete({room_Id: req.params.room_Id});
-        res.status(200).json({message : "Success",deletedRoom});
-    }catch(err){
-        res.status(500).json({message:"Not Found"})
-    }
-})
+router.delete('/:room_Id', deleteGlobalRoom);
 
 module.exports = router;
