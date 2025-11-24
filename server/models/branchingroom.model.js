2b068162d87e7bd1d48eef666e49a0f1d676d129
Abdullah Arif <arifab@chalmers.se>
2025-11-24 15:45:04 +0100

    #7 Update the Schema with new room topics

--- server/models/branchingroom.model.js
+++ server/models/branchingroom.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const branchingRoomSchema = new mongoose.Schema({
     branchingRoomId : {type : String, unique: true, required: true, immutable : true},
-    roomTopic : {type: String, enum:["School", "Hobbies", "Food"] ,required : true, },
+    roomTopic : {type: String, enum:["General", "Scandle", "Relationships", "Travel","Movies", "Books", "Sports", "Food", "School"] ,required : true, },
     branchingRoomType: {type: String, enum: ["LocalRoom", "GlobalRoom"], required: true, immutable : true},
 
     //Foreign key Refference to one of the super type(Local, Global) so that we can track the users. 
