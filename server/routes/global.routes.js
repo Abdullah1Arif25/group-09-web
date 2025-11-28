da1bcace1ee2e89d3ad749c3218779aadb588b16
sireen <ajamieh@student.chalmers.se>
2025-11-28 09:22:12 +0100

    #4 Fix endpoint naming in global

--- server/routes/global.routes.js
+++ server/routes/global.routes.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const {createGlobalRoom, getGlobalRoom, updateGlobalRoom, deleteGlobalRoom} = require('../controllers/global.controllers');
 
-// POST /api/global
+// POST /api/globalroom
 router.post('/', createGlobalRoom);
 
-// GET /api/globalRoom
+// GET /api/globalroom
 router.get('/', getGlobalRoom);
 
-// PUT /api/live_chat
+// PUT /api/globalroom/:room_Id
 router.put('/:room_Id', updateGlobalRoom);
 
-// Delete /api/Global/:room_Id
+// Delete /api/globalroom/:room_Id
 router.delete('/:room_Id', deleteGlobalRoom);
 
 module.exports = router;
