d14f161ca34c0ad52aa5f21118752a9e9d658801
Abdullah Arif <arifab@chalmers.se>
2025-12-04 21:02:39 +0100

    #16 Add path to Local Room Component

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
+import LocalRoom from './views/Localroom.vue' 
 
 const routes = [
-  { path: '/', name: 'home', component: Home }
+  { path: '/', name: 'home', component: Home },
+  { path: '/localroom', name: 'localroom', component: LocalRoom },
 ]
 
 const router = createRouter({
