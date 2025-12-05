af08cc2f1e58716d1e535f7583729fc81a48eea8
sireen <ajamieh@student.chalmers.se>
2025-12-05 01:29:27 +0100

    #14 Modify Register path

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>
-      <router-link to="/register">Register</router-link>
     </div>
     <!-- Render the content of the current page view -->
     <router-view/>
