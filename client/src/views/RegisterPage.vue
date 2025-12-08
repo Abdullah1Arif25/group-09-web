2f8a259450a02a054620f9203f48830c8d1603fd
Aisha Attar <aishaat@student.chalmers.se>
2025-12-08 16:16:53 +0100

    #19 Add PasswordPage.vue with layout, formatting and styling. Add the page route as well.

--- client/src/views/RegisterPage.vue
+++ client/src/views/RegisterPage.vue
@@ -57,7 +57,7 @@ export default {
   methods: {
     async handleRegister() {
       try {
-        await Api.post("/auth/register", {
+        await Api.post("/users/register", {
           userId: this.userId,
           personalNumber: this.personalNumber,
           language: this.language,
