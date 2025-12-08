d77e5ba41fb14f6edc0f05f50cf62d82437de63b
sireen <ajamieh@student.chalmers.se>
2025-12-08 11:21:26 +0100

    #18 Add main page and styling

--- client/src/router.js
+++ client/src/router.js
@@ -4,12 +4,14 @@ import Home from './views/Home.vue'
 import RegisterPage from './views/RegisterPage.vue'
 import LoginPage from './views/LoginPage.vue'
 import Localroom from './views/Localroom.vue'
+import MainPage from './views/MainPage.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/login', name: 'login', component: LoginPage },
   { path: '/register', name: 'Register', component: RegisterPage },
-  { path: '/localroom', name: 'localroom', component: Localroom }
+  { path: '/localroom', name: 'localroom', component: Localroom },
+  { path: '/main', name: 'main', component: MainPage },
 
 ]
 
