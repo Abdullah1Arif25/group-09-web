fc04237f7d1d214a208e5ac905221b9f9fad9b57
Sireen Abu Ajamieh <ajamieh@student.chalmers.se>
2025-12-08 11:03:16 +0000

    Add router for main page

--- client/src/router.js
+++ client/src/router.js
@@ -4,13 +4,14 @@ import Home from './views/Home.vue'
 import RegisterPage from './views/RegisterPage.vue'
 import LoginPage from './views/LoginPage.vue'
 import LocalRoom from './views/LocalRoom.vue'
+import MainPage from './views/MainPage.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/login', name: 'login', component: LoginPage },
   { path: '/register', name: 'Register', component: RegisterPage },
-  { path: '/localroom', name: 'localroom', component: LocalRoom }
-
+  { path: '/localroom', name: 'localroom', component: LocalRoom },
+  { path: '/main', name: 'main', component: MainPage },
 ]
 
 const router = createRouter({
