df1967ea247315135f5b145c13c39b91190be304
Sireen Abu Ajamieh <ajamieh@student.chalmers.se>
2025-12-19 15:48:29 +0300

    #24 Add display users to admin

--- client/src/router.js
+++ client/src/router.js
@@ -8,6 +8,7 @@ import MainPage from './views/MainPage.vue'
 import PasswordPage from './views/PasswordPage.vue'
 import ProfilePage from './views/ProfilePage.vue';
 import AdminPage from './views/AdminPage.vue';
+import UsersPage from "./views/UsersPage.vue";
 
 
 const routes = [
@@ -19,6 +20,7 @@ const routes = [
   { path: '/password', name: 'password', component: PasswordPage },
   { path: '/profile', name: 'profile', component: ProfilePage },
   { path: '/admin', name: 'admin', component: AdminPage },
+  { path: "/admin/users", name: "admin-users",component: UsersPage },
 ]
 
 const router = createRouter({
